64ad758eaa8f9437ec23a387f75cfb42c853666d
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2024-01-12 19:25:43 -0300

    002

---
+++ ex002.py
@@ -0,0 +1,7 @@
+#user input
+
+usersName = input("What is your name?")
+print('Your name is', usersName)
+
+usersAge = input("What is your age?")
+print('Then your age is {} and your name is {}'.format(usersAge, usersName))
