5b94a96542f53bdcc59ae473505208bf99a40691
Victor Feliciano <115307935+VetVictor33@users.noreply.github.com>
2024-01-12 21:13:18 -0300

    feat: new string format syntax

--- ex012.py
+++ ex012.py
@@ -7,5 +7,6 @@ deserved_raise = int(input('Tell us your percentage expectation: '))
 
 new_salary = give_me_a_raise(old_salary, deserved_raise)
 
-print('Because you are a good asset to our company we are giving you a much deserved {}% raise, which is ${} more than your old salary, making it now ${} '.
-      format(deserved_raise, new_salary - old_salary, new_salary))
+# print('Because you are a good asset to our company we are giving you a much deserved {}% raise, which is ${} more than your old salary, making it now ${} '.
+#       format(deserved_raise, new_salary - old_salary, new_salary))
+print(f'Because you are a good asset to our company we are giving you a much deserved {deserved_raise}% raise, which is ${new_salary - old_salary} more than your old salary, making it now ${new_salary} ')
